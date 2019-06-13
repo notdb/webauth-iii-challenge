67535af52c08017468b6e9c47ed850966b02f2d8
Doug <me@douglasbowen.com>
2019-06-13 18:58:22 -0400

    add login and logout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { NavLink, Route, withRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+import Login from "./components/Signup.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <NavLink to="/login">Login</NavLink>
+            <button onClick={this.logout}>Logout</button>
+          </nav>
+        </header>
+        <main>
+          <Route path="/login" component={Login} />
+        </main>
+      </div>
+    );
+  }
+  logout = () => {
+    localStorage.removeItem("jwt");
+    this.props.history.push("/login");
+  };
 }
 
-export default App;
+export default withRouter(App);
